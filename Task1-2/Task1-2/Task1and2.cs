549afdec2a0476a90071ad2aa61638c10864ea46
davidyc <davydovsergey.sd@gmail.com>
2018-11-07 11:22:51 +0600

    Task1-2 with comments

--- Task1-2/Task1-2/Task1and2.cs
+++ Task1-2/Task1-2/Task1and2.cs
@@ -8,6 +8,13 @@ namespace Task1_2
 {
     public class Task1and2
     {
+        /// <summary>
+        /// Search for the maximum number
+        /// </summary>
+        /// <param name="arr">int Array</param>
+        /// <param name="max">Current max</param>
+        /// <param name="i">item number to compare</param>
+        /// <returns>Max from array</returns>
         public static int MaxInArray(int[] arr, int max, int i = 0)
         {
             if (i >= arr.Length)
@@ -15,8 +22,12 @@ namespace Task1_2
             return MaxInArray(arr, arr[i] > max ? arr[i] : max, i + 1);
         }
 
-
-        public static double Method2(double[] array)
+        /// <summary>
+        /// Search index of the element for which the sum of the elements on the left and the sum of the elements of the parameter are equal.
+        /// </summary>
+        /// <param name="array">Double array</param>
+        /// <returns>Number Index</returns>
+        public static double IndexEquelsLeftRigth(double[] array)
         {
             double leftSum = 0;
             double rigthSum = 0;
