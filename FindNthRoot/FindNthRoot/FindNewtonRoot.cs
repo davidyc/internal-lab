38cbcf9eb2c445f242fedcaba609a6789b91dd33
davidyc <davydovsergey.sd@gmail.com>
2018-11-08 10:18:44 +0600

    Newton with Tests

---
+++ FindNthRoot/FindNthRoot/FindNewtonRoot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNthRoot
+{
+    public static class FindNewtonRoot
+    {
+        /// <summary>
+        /// Find Root method newton
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="root"></param>
+        /// <param name="eps"></param>
+        /// <returns></returns>
+        public static double NewtonRoot(double number, int root, double eps)
+        {
+            if ((number <= 0 && root % 2 == 0) || root <= 0)
+            {
+                return 0;
+            }
+
+            if (eps <= 0 || eps >= 1)
+            {
+                return 0;
+            }
+
+            double tmp1 = number / root;
+            double tmp2 = 1.0 / root * ((root - 1) * tmp1 + number / Math.Pow(tmp1, root - 1));
+            while (Math.Abs(tmp1 - tmp2) > eps)
+            {
+                tmp1 = tmp2;
+                tmp2 = 1.0 / root * ((root - 1) * tmp1 + number / Math.Pow(tmp1, root - 1));
+            }
+            return tmp2;
+
+        }
+
+
+
+    }
+}
---
+++ FindNthRoot/UnitTestNewton/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FindNthRoot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestNewton
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// [TestCase(1, 5, 0.0001,ExpectedResult =1)]       
+        /// </summary>
+        [TestMethod]
+        public void TestCase1()
+        {
+            double test;
+            test = FindNewtonRoot.NewtonRoot(1.0, 5, 0.0001);
+
+            Assert.AreEqual(test, 1.0, 1);
+
+        }
+
+        /// <summary>
+        /// [TestCase(8, 3, 0.0001, ExpectedResult = 2)]     
+        /// </summary>
+        [TestMethod]
+        public void TestCase2()
+        {
+            double test;
+            test = FindNewtonRoot.NewtonRoot(8, 3, 0.0001);
+
+            Assert.AreEqual(test, 2.0, 1);
+
+        }
+
+
+        /// <summary>
+        /// [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]    
+        /// </summary>
+        [TestMethod]
+        public void TestCase3()
+        {
+            double test;
+            test = FindNewtonRoot.NewtonRoot(0.001, 3, 0.0001);
+
+            Assert.AreEqual(test, 0.1, 1);
+
+        }
+
+        
+    }
+}
