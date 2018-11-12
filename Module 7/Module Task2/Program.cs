69724861b8ffc7d120c3ce3a5a57e523ddaa3597
davidyc <davydovsergey.sd@gmail.com>
2018-11-12 20:47:31 +0600

    Module  7  task 2 with tests

--- Module 7/Module Task2/Program.cs
+++ Module 7/Module Task2/Program.cs
@@ -9,52 +9,13 @@ namespace Module_Task2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {      
+            Console.WriteLine(TitleSenten.ConvertToTitleSentence("a clash of KINGS", "a an the of"));
+            Console.WriteLine(TitleSenten.ConvertToTitleSentence("THE WIND IN THE WILLOWS", "in The"));
+            Console.WriteLine(TitleSenten.ConvertToTitleSentence("the quick brown fox"));
 
-            string test = "a clash of KINGS";
-
-            Console.WriteLine(ConvertToTitleSentence(test));
-
-            Console.ReadKey();            
-        }
-
-
-        public static string ConvertToTitleSentence(string sentence, string exception = "")
-        {
-            string tmp = "";
-            string exp = "";
-
-            tmp = sentence.ToLower();
-            exp = exception.ToLower();
-
-            tmp = "";
-
-            string[] xx = tmp.Split(' ');
-            string[] arrayExp = exception.Split(' ');
-
-
-
-            for (int i = 0; i < xx.Length; i++)
-            {
-                for (int j = 0; j < arrayExp.Length; j++)
-                {
-                    if(arrayExp[i].ToLower() == xx[j].ToLower())
-                    {
-                        tmp += xx[i] + " ";
-                    }
-                    else
-                    {
-                        tmp += xx[i].First().ToString().ToUpper() + xx[i].Substring(1) + " ";
-                    }
-                   
-                }
-               
-            }
-           
-
-            return tmp;
-        }
-
+            Console.ReadKey();
+        }       
     }
+    
 }
---
+++ Module 7/Module Task2/TitleSenten.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Task2
+{
+    public class TitleSenten
+    {
+        /// <summary>
+        /// Make sentenses Title
+        /// </summary>
+        /// <param name="sentence">Sentenses</param>
+        /// <param name="exception">expception</param>
+        /// <returns></returns>
+        public static string ConvertToTitleSentence(string sentence, string exception = "")
+        {
+            StringBuilder tmp = new StringBuilder();
+
+            string[] arrayOfWord = sentence.Split(' ');
+            string[] arrayOfExp = exception.Split(' ');
+
+            for (int i = 0; i < arrayOfWord.Length; i++)
+            {
+                bool chaenge = true;
+                for (int j = 0; j < arrayOfExp.Length; j++)
+                {
+                    if (arrayOfWord[i].ToLower() == arrayOfExp[j].ToLower())
+                    {
+                        chaenge = false;
+                        break;
+                    }
+                }
+
+                if (chaenge == true || i == 0)
+                {
+                    arrayOfWord[i] = arrayOfWord[i].ToLower();
+                    tmp.Append(arrayOfWord[i].First().ToString().ToUpper() + arrayOfWord[i].Substring(1) + " ");
+                }
+                else
+                {
+                    arrayOfWord[i] = arrayOfWord[i].ToLower();
+                    tmp.Append(arrayOfWord[i] + " ");
+                }
+            }
+
+            return tmp.ToString();
+        }
+    }
+}
