d542caf8e88d23b079807981896a2d76e94c5df5
davidyc <davydovsergey.sd@gmail.com>
2018-12-10 16:44:14 +0600

    at

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,38 +1,50 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace HelloApp
 {
     class Program
-    {
-        static void Factorial()
-        {
-            int result = 1;
-            for (int i = 1; i <= 6; i++)
-            {
-                result *= i;
-            }
-            Thread.Sleep(8000);
-            Console.WriteLine($"Факториал равен {result}");
-        }
-        // определение асинхронного метода
-        static async void FactorialAsync()
-        {
-            Console.WriteLine("Начало метода FactorialAsync"); // выполняется синхронно
-            await Task.Run(Factorial);                         // выполняется асинхронно
-            Console.WriteLine("Конец метода FactorialAsync");  // выполняется синхронно
-        }
-
+    {     
         static void Main(string[] args)
         {
-            FactorialAsync();   // вызов асинхронного метода
-
-            Console.WriteLine("Введите число: ");
-            int n = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"Квадрат числа равен {n * n}");
+            MyClass tmp = new MyClass();
+            int[] array = tmp.Mymethod();
 
             Console.Read();
+        }
+    }
+
+    class MyAt : Attribute
+    {
+        public int Count { get; set; }
+    }
+
+    [MyAt(Count = 10)]
+    class MyClass
+    {
+        int field;
+
+        public int MyProperty { get; set; }
+
+        public MyClass()
+        {
+
+        }
+
+        public int[] Mymethod()
+        {
+            var type = this.GetType();
+            if (Attribute.IsDefined(type, typeof(MyAt)))
+            {
+                var countAt = Attribute.GetCustomAttribute(type, typeof(MyAt)) as MyAt;
+                return new int[countAt.Count];
+            }
+            else
+                return new int[0];
         }
     }
 }
