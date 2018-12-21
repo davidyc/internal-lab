75a67f997d65f4bc99e64d0d47cd63eedf91ce74
davidyc <davydovsergey.sd@gmail.com>
2018-12-21 10:42:25 +0600

    Done

--- Module 21-22/Module 21-22/BookStore.cs
+++ Module 21-22/Module 21-22/BookStore.cs
@@ -8,23 +8,33 @@ namespace Module_21_22
 {
     class BookStore
     {
+        /// <summary>
+        /// Add new author 
+        /// </summary>
+        /// <param name="authorName">Name author</param>
         public static void AddAuthor(string authorName)
         {
             using(BookstoreEntities db = new BookstoreEntities())
             {
-                db.Authors.Add(new Authors { name = authorName });
+                db.Authors.Add(new Author { name = authorName });
                 db.SaveChanges();  
             }
         }
 
+        /// <summary>
+        /// Add new book 
+        /// </summary>
+        /// <param name="bookName">Name book</param>
+        /// <param name="idAuthor">id autho</param>
+        /// <param name="BookPrice">price book</param>
         public static void AddBook(string bookName, int idAuthor, int BookPrice)
         {
             using (BookstoreEntities db = new BookstoreEntities())
             {
-                Authors author =  db.Authors.FirstOrDefault(x=>x.id==idAuthor);
+                Author author =  db.Authors.FirstOrDefault(x=>x.id==idAuthor);
                 if(author != null)
                 {
-                    db.Books.Add(new Books { name = bookName, price = BookPrice, Authors = author });
+                    db.Books.Add(new Book { name = bookName, price = BookPrice, Author = author });
                     db.SaveChanges();
                 }
                 else
@@ -32,26 +42,37 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Add new customer 
+        /// </summary>
+        /// <param name="customerName">Customer name</param>
+        /// <param name="customerAdress">customer adress</param>
         public static void AddCustomer(string customerName, string customerAdress)
         {
             using (BookstoreEntities db = new BookstoreEntities())
             {
-               db.Customers.Add(new Customers { name = customerName, adress = customerAdress });
+               db.Customers.Add(new Customer { name = customerName, adress = customerAdress });
                db.SaveChanges();              
             }
+            
         }
 
+        /// <summary>
+        /// Add new Order
+        /// </summary>
+        /// <param name="idB">book id</param>
+        /// <param name="idCust">customer id</param>
         public static void AddOrder(int idB, int idCust)
         {
             using (BookstoreEntities db = new BookstoreEntities())
             {
-                Books book = db.Books.FirstOrDefault(x => x.id == idB);
-                Customers customer = db.Customers.FirstOrDefault(x => x.id == idCust);
+                Book book = db.Books.FirstOrDefault(x => x.id == idB);
+                Customer customer = db.Customers.FirstOrDefault(x => x.id == idCust);
 
 
                 if (customer != null & book != null)
                 {
-                    db.Orders.Add(new Orders { idBook = idB, idCustomer = idCust });
+                    db.Orders.Add(new Order { idBook = idB, idCustomer = idCust });
                     db.SaveChanges();
                 }
                 else
@@ -59,6 +80,11 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Chenged author name
+        /// </summary>
+        /// <param name="newName">new name</param>
+        /// <param name="_id">id</param>
         public static void EditAuthor(string newName, int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
@@ -74,6 +100,11 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Chenged book price for id book
+        /// </summary>
+        /// <param name="_id">id book</param>
+        /// <param name="newPrice">new price</param>
         public static void EditBook(int _id, int newPrice)
         {
             using (BookstoreEntities db = new BookstoreEntities())
@@ -89,6 +120,11 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// New name customer for id book
+        /// </summary>
+        /// <param name="newName">new name customer</param>
+        /// <param name="_id">id book</param>
         public static void EditCustomer(string newName, int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
@@ -105,6 +141,11 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Chenged id book in order id
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <param name="newBookID"></param>
         public static void EditOrder(int _id, int newBookID)
         {
             using (BookstoreEntities db = new BookstoreEntities())
@@ -114,7 +155,7 @@ namespace Module_21_22
 
                 if (tmp != null && tmpBook != null)
                 {
-                    tmp.Books = tmpBook;
+                    tmp.Book = tmpBook;
                     db.SaveChanges();
                 }
                 else
@@ -123,6 +164,10 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Delete authot id with all his books and order this books
+        /// </summary>
+        /// <param name="_id">id authors</param>
         public static void DeleteAuthor(int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
@@ -131,22 +176,48 @@ namespace Module_21_22
 
                 if (tmp != null)
                 {
-                  
+                    var listOfBooksForDelete = db.Books.Where(x => x.AuthorID == _id);
+                    foreach (var item in listOfBooksForDelete)
+                    {
+                        DeleteBook(item.id);
+                    }                                                  
+                }            
+                else
+                    Console.WriteLine("Error");                
+            }
+
+            using (BookstoreEntities db = new BookstoreEntities())
+            {
+                var tmp = db.Authors.FirstOrDefault(x => x.id == _id);
+
+                if (tmp != null)
+                {                   
+                    db.Authors.Remove(tmp);
                     db.SaveChanges();
                 }
                 else
                     Console.WriteLine("Error");
             }
         }
-
+     
+        /// <summary>
+        /// Delete id with all orders book
+        /// </summary>
+        /// <param name="_id"></param>
         public static void DeleteBook(int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
             {
                 var tmp = db.Books.FirstOrDefault(x => x.id == _id);
+
                 if (tmp != null)
                 {
-         
+                    var listForDelete = db.Orders.Where(x => x.idBook == _id);
+                    foreach (var item in listForDelete)
+                    {
+                        db.Orders.Remove(item);
+                    }
+                    db.Books.Remove(tmp);       
                     db.SaveChanges();
                 }
                 else
@@ -154,24 +225,35 @@ namespace Module_21_22
             }
         }
 
+        /// <summary>
+        /// Delete customer with all his orders
+        /// </summary>
+        /// <param name="_id">customer id</param>
         public static void DeleteCustomer(int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
             {
                 var tmp = db.Customers.FirstOrDefault(x => x.id == _id);
 
-
                 if (tmp != null)
                 {
-               
+                    var listForDelete =  db.Orders.Where(x => x.idCustomer == _id);
+                    foreach (var item in listForDelete)
+                    {
+                        db.Orders.Remove(item);
+                    }
+                    db.Customers.Remove(tmp); 
                     db.SaveChanges();
                 }
                 else
                     Console.WriteLine("Error");
-
             }
         }
 
+        /// <summary>
+        /// Delete order id
+        /// </summary>
+        /// <param name="_id">order  ID</param>
         public static void DeleteOrder(int _id)
         {
             using (BookstoreEntities db = new BookstoreEntities())
--- Module 21-22/Module 21-22/Program.cs
+++ Module 21-22/Module 21-22/Program.cs
@@ -10,19 +10,20 @@ namespace Module_21_22
     {
         static void Main(string[] args)
         {
-            //BookStore.AddAuthor("Athuor");
-            //BookStore.AddBook("Books", 1, 1000);
-            //BookStore.AddBook("Boo", 1, 100);
-            // BookStore.AddCustomer("Customer", "Adress");
-            //BookStore.AddOrder(1, 1);
+            //BookStore.AddAuthor("Author for Delete");
+            //BookStore.AddBook("Book for delete", 3, 1000); // id = 3 так как в базе уже есть 2 автора
+            //BookStore.AddCustomer("Customer for delete", "Delete adress");
+            //BookStore.AddOrder(2, 2);
 
             //BookStore.EditAuthor("newName", 1);
             //BookStore.EditBook(1, 5000);
             //BookStore.EditCustomer("NewName", 1);
             //BookStore.EditOrder(1, 2);
 
-
-
+            //BookStore.DeleteOrder(3);
+            //BookStore.DeleteCustomer(3);
+            //BookStore.DeleteBook(4);
+            //BookStore.DeleteAuthor(5);
 
 
             Console.Read();
